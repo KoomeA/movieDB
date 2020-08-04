b1ace023fe03fbf219d5bbfcd6c45afe4807aa20
kelvin-daniel <kaymutor@gmail.com>
2020-08-04 21:24:56 +0300

    add movies search output div

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e)=> {
         let searchText=$('#searchText').val();
-        getmovies(searchText);
+        getMovies(searchText);
         e.preventDefault();
     })
 });
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com?s=' = searchText)
+    .then((response)=> {
+        let movies =response.data.search;
+        let output ='';
+        $.each(movies, (index, movie)=>{
+            output +=`
+            <div class="col-md-3">
+                <div class="well text-center">
+                    <img src="${movie.Poster}">
+                    <h5>${movie.Title}</h5>
+                    <a onclick="movieSelected('${movie.Imdb}')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+            </div>
+            `;
+        });
+        $('#movies').html(output);
+    })
+    .catch((err) => {
+        alert(":( sorry, didin't find anything matching that")
+    });
+}
