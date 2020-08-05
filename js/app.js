8c137b7acc9d239ff1d65c93c5306053b01816d2
Edward Koome <koomeedward77@gmail.com>
2020-08-05 14:39:23 +0300

    add a Readme file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#notReg").click(function(){
+        $("#register").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#log").click(function(){
+        $(".login").toggle();
+    });
+});
