edd6711411595860a68a58d61ff8fbbea39dca81
Edward Koome <koomeedward77@gmail.com>
2020-08-06 09:37:03 +0300

    change login details

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
         $(".login").toggle();
     });
 });
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg =document.getElementById("login-error");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+});
